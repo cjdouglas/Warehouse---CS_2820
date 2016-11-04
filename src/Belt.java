fd38a3c33833c403120b9f86fe982cee4006347f
LGrundUIowa <leon-grund@uiowa.edu>
2016-11-04 10:13:30 -0500

    Added fields and methods(getters)
    belt moves
    number of bins on belt

--- src/Belt.java
+++ src/Belt.java
@@ -1,4 +1,13 @@
 // Leon Grund
 public class Belt {
 
+	private boolean moves = false;
+	private int numBins;
+	
+	//getters
+	public boolean isMoving(){return moves;}
+	public int numOfBins(){return numBins;}
+	
+	
+	
 }
