5e58bda72898e47fbe462d12e993bc50ff6811b7
theliamcrawford <liam.crawford@outlook.com>
2016-11-06 23:50:26 -0600

    Update MasterController

--- src/warehouse/MasterController.java
+++ src/warehouse/MasterController.java
@@ -1,5 +1,39 @@
 package warehouse;
 
+/** @author Liam Crawford
+ * 
+ * Group 2A - Master Class
+ *
+ */
+
+/**
+ * Tasks of Master Controller:
+ * Creates an instance of inventory by handing it an file of items
+ * Creates an instance of Floor with the number of shelves by querying the created inventory
+ * Creates a robot scheduler by handing it said floor and an arbitrary number of robots
+ * Creates a visulizer by handing it the robot scheduler
+ *
+ */
+
 public class MasterController {
-	int x = 9;
+	// initialize the components
+	MockInventory MI;
+	Floor F;
+	RobotScheduler RS;
+	MockVisualizer MV;
+	
+	MasterController(String inventoryFile, int numRobots){
+		MI = new MockInventory(inventoryFile);
+		F = new Floor(MockInventory.getNumShelves());
+		RS = new RobotSheduler(F ,numRobots);
+	}
+	
+	public class MockInventory {
+		MockInventory(String filename){}
+		public int getNumShelves(){return 30;}
+	}
+	
+	public class MockVisualizer{
+		MockVisualizer(RobotScheduler RS){}
+	}
 }
