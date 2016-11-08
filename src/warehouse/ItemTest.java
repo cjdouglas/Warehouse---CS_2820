4e487b230c986c0df842dae21b6549ad6fa7fe82
LGrundUIowa <leon-grund@uiowa.edu>
2016-11-07 23:47:03 -0600

    Test Items for Belt
    create Items (bins or packages) to test Belt

---
+++ src/warehouse/ItemTest.java
@@ -0,0 +1,27 @@
+// Leon Grund
+public class ItemTest {
+	private boolean bin;
+	private boolean packag;
+	private int ID;
+	private int x,y;
+	private String progress;
+	
+	public ItemTest(int item, int id){
+		if(item == 0){bin = true;}
+		if(item == 1){packag = true;}
+		//ID = id;
+	}
+	
+	//setters
+	public void setID(int id){ID = id;}
+	public void updateLocation(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+	public void setProgress(String prg){progress = prg;}
+	
+	//getters
+	public int getID(){return ID;}
+	public int[] getLocation(){return new int[] {x,y};}
+	public String getProgess(){return progress;}
+}
