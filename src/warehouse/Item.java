0fcc5f5c0791aaca1fabfdc3ada8f73af1fd4e1a
LGrundUIowa <leon-grund@uiowa.edu>
2016-11-15 01:01:38 -0600

    Added Location for belt
    created getter and setter to keep track of item's location on belt

--- src/warehouse/Item.java
+++ src/warehouse/Item.java
@@ -1,20 +1,69 @@
 package warehouse;
 
 import java.awt.Point;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author sethpratt
+ *
+ */
 
 public class Item {
-	final int itemID;
+	int itemID;
 	public String name;
 	public double price;
-	final int weight;
+	int weight;
 	int numStocked;
 	public boolean reStock;
+	int amazonID;
+	InventoryCatalog inventory = new InventoryCatalog();
+	CatItem catitem;
+	static ArrayList<Item> itemList;
+	Point location;
 
+	/*
+	 * Constructor for creating a new Item
+	 */
 	public Item(int itemID, String name, double price, int weight) {
 		this.itemID = itemID;
 		this.name = name;
 		this.price = price;
 		this.weight = weight;
+	}
+	
+	// Constructor added for belt testing
+	public Item(int itemID){this.itemID = itemID;}
+	
+	/*
+	 * @author Seth Pratt
+	 */
+
+	public Item(int amazonID, int id, String description) {
+		for (int i = 0; i < catitem.getcalalog().length; i++) {
+			amazonID = i;
+			id = catitem.getID();
+			description = catitem.getDescription();
+		}
+	}
+
+	/*
+	 * Create a list of items from the item catalog
+	 */
+	void buildItemList(CatItem[] catalog) {
+		for (int i = 0; i < catalog.length; i++) {
+			itemList.add(new Item(i, catalog[i].getID(), catalog[i].getDescription()));
+		}
+	}
+
+	/*
+	 * Display list of items
+	 */
+	protected static void displayItemList() {
+		System.out.println("Item List: ");
+		for (Item item : itemList) {
+			System.out.print(item + ", ");
+		}
 	}
 
 	// Objectives
@@ -26,7 +75,11 @@ public class Item {
 	 */
 
 	// GETTERS
-	public int getItemID() {
+	int getAmazonItem() {
+		return this.amazonID;
+	}
+
+	int getItemID() {
 		return this.itemID;
 	}
 
@@ -47,19 +100,47 @@ public class Item {
 		return item.numStocked;
 	}
 
+	// tracks the location of item on belt
+	public Point getLocation() {
+		return location;
+	}
+	
 	// SETTERS
 	void setPrice(double newPrice) {
 		this.price = newPrice;
 	}
-
-	public Point getLocation() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	// updates the location of item on belt
+	public void setLocation(Point loc) {
+		location = loc;
 	}
+	
 
-	public void setLocation(Point packLocation) {
-		// TODO Auto-generated method stub
-		
+
+	public static void main(String[] args) {
+		// String fileName = "ItemsList.csv";
+		// File file = new File(fileName);
+		// try {
+		// Scanner inputStream = new Scanner(file);
+		// inputStream.next(); // Disregard first line
+		// while (inputStream.hasNext()) {
+		// String data = inputStream.next();
+		// // data = data.replaceAll("\\s+", "");
+		// String[] ItemParams = data.split(",");
+		// int itemID = Integer.parseInt(ItemParams[0]);
+		// String name = ItemParams[1];
+		// double price = Double.parseDouble(ItemParams[2]);
+		// double weight = Double.parseDouble(ItemParams[3]);
+		// // System.out.println(ItemParams[0]);
+		// }
+		// inputStream.close();
+		//
+		// } catch (FileNotFoundException e) {
+		// e.printStackTrace();
+		// }
+
+		displayItemList();
+
 	}
 
 }
